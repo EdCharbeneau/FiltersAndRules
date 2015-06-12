b91a5d182043b3fe644ef40deda6ac53b202be4e
Ed Charbeneau <ed.charbeneau@telerik.com>
2015-06-12 12:43:37 -0400

    Setup test project -Added Model -Added Xunit -Added FluentAssertions

---
+++ FiltersAndRules/Model/Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FiltersAndRules.Model
+{
+    public class Post
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Title { get; set; }
+
+        public string ContentEncoded { get; set; }
+
+        public virtual string ShortDescription { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual string Meta { get; set; }
+
+        public virtual string UrlSlug { get; set; }
+
+        public virtual bool IsPublished { get; set; }
+
+        public virtual DateTime PostedOn { get; set; }
+
+        public virtual DateTime? Modified { get; set; }
+
+        public string Category { get; set; }
+
+        public virtual IList<Tag> Tags { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
